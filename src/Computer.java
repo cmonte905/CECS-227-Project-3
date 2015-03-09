285bba4efc2f91f7e440a9c5035c9734ca5cfbef
cmonte905 <cmonte905@hotmail.com>
2015-03-08 20:30:32 -0700

    Some more work

--- src/Computer.java
+++ src/Computer.java
@@ -1,4 +1,11 @@
 
 public class Computer {
+	public Computer(){
+		
+	}
+	public int hashCode() {
+		String hashStr = "1";
+		return hashStr.hashCode();
+		}
 
 }
--- src/MainGame.java
+++ src/MainGame.java
@@ -2,34 +2,46 @@ import java.util.*;
 
 public class MainGame {
 	public static void main(String[] args) {
-
+		Computer c = new Computer();
 		Scanner in = new Scanner(System.in);
 		Random rand = new Random();
 		boolean gameState = true;
 		char playerValue;
 		char computerValue = 0;
 		int cChoice = 0;
-		System.out
-				.println("Rock Paper Sciccors\n1: Rock\n2: Paper\n3: Sciccors");
-		int usersChoice = in.nextInt();
-
+		char cont = ' ';
+		String preditions = "";
 		while (gameState) {
-
+			System.out
+					.println("Rock Paper Sciccors\n1: Rock\n2: Paper\n3: Sciccors");
+			int usersChoice = in.nextInt();
 			cChoice = rand.nextInt(3) + 1;
 
 			playerValue = assignValue(usersChoice);
 			computerValue = assignValue(cChoice);
+			int winOrLose = winLogic(playerValue, computerValue);
+			if (winOrLose == 1) {
+				System.out.println("You win!!!");
+			} else if (winOrLose == -1) {
+				System.out.println("You lose");
+			} else {
+				System.out.println("Its a tie");
+			}
+			preditions += playerValue;
+			
+
 			boolean validIn = true;
 			while (validIn) {
 				System.out.println("Would you like to continue? y/n");
-				String cont = in.next();
-				if (cont.contentEquals("y") || cont.contentEquals("Y")) {
+				 cont = in.nextLine().charAt(0);
+				if (cont == 'y' || cont == 'Y') {
+					gameState = false;
 					validIn = false;
 				} else {
 					validIn = true;
 				}
 			}
-
+			System.out.println(preditions);
 		}
 	}
 
@@ -48,6 +60,32 @@ public class MainGame {
 
 		}
 		return value;
+	}
+
+	public static int winLogic(char Player, char Comp) {
+
+		if (Player == 'R' && Comp == 'S') {
+			return 1;
+			// player wins rock beats sci
+		} else if (Player == 'S' && Comp == 'R') {
+			return -1;
+			// computer wins rock beats sci
+		} else if (Player == 'R' && Comp == 'P') {
+			return -1;
+			// computer wins Paper beats rock
+		} else if (Player == 'P' && Comp == 'R') {
+			return 1;
+			// player wins Paper beats rock
+		} else if (Player == 'P' && Comp == 'S') {
+			return -1;
+			// computer wins Sci beats paper
+		} else if (Player == 'S' && Comp == 'P') {
+			return 1;
+			// player wins Sci beats paper
+		} else {
+			return 0;
+			// tie
+		}
 	}
 
 }
